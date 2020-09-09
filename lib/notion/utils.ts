0dbd8acf270266098b9deb0780aadb9b1ae78b6d
kongjiacong <kongjiacong@bytedance.com>
2020-09-09 12:25:09 +0800

    chore

--- lib/notion/utils.ts
+++ lib/notion/utils.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export function setHeaders(req: NextApiRequest, res: NextApiResponse): boolean {
   // set SPR/CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
+  res.setHeader('Cache-Control', 's-maxage=100, stale-while-revalidate')
   res.setHeader('Access-Control-Allow-Methods', 'GET')
   res.setHeader('Access-Control-Allow-Headers', 'pragma')
 
